a2c8b73dbcd6c832119caf356b27f12cdfda76eb
gladkoff07 <t-orange@yandex.ru>
2025-06-30 16:31:49 +0300

    correct scss, js code

--- src/js/main.js
+++ src/js/main.js
@@ -1,4 +1,30 @@
 document.addEventListener("DOMContentLoaded", function () {
 
-  console.log('DOM loaded!')
+  // Fancybox init
+  Fancybox.bind("[data-fancybox]", {
+    animationEffect: "zoom-in-out",
+    slideClass: "modal-close",
+  })
+
+  // Swiper init
+  const swiperName = new slider.Swiper(".js-slider-name", {
+    // configure Swiper to use modules
+    modules: [slider.Navigation, slider.Pagination],
+
+    slidesPerView: 2,
+    roundLengths: true,
+    loop: true,
+
+    // If we need pagination
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+
+    // Navigation arrows
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+  })
 })
--- src/js/vendor.js
+++ src/js/vendor.js
@@ -1,33 +1,8 @@
 // Import Libs from npm
-
 import { Fancybox } from "@fancyapps/ui"
 import Swiper from "swiper"
 import { Navigation, Pagination } from "swiper/modules"
 
-// Fancybox init
-Fancybox.bind("[data-fancybox]", {
-  animationEffect: "zoom-in-out",
-  slideClass: "modal-close",
-})
-
-// Swiper init
-const swiperName = new Swiper(".js-slider-name", {
-  // configure Swiper to use modules
-  modules: [Navigation, Pagination],
-
-  slidesPerView: 3,
-  roundLengths: true,
-  loop: true,
-
-  // If we need pagination
-  pagination: {
-    el: ".swiper-pagination",
-    clickable: true,
-  },
-
-  // Navigation arrows
-  navigation: {
-    nextEl: ".swiper-button-next",
-    prevEl: ".swiper-button-prev",
-  },
-})
+// add global variables
+window.slider = {Swiper, Navigation, Pagination}
+window.Fancybox = Fancybox
