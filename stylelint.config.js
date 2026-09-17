bc63254a37dd4ebd4d49ae0301007af94872e2bb
gladkoff07 <t-orange@yandex.ru>
2026-09-17 11:45:05 +0300

    correct stylelint.config file

--- stylelint.config.js
+++ stylelint.config.js
@@ -1,4 +1,11 @@
 /** @type {import('stylelint').Config} */
 export default {
   extends: ['stylelint-config-standard-scss'],
+  rules: {
+    // Конвенция проекта: camelCase с необязательным суффиксом через
+    // подчёркивание ($colorSmokyWhite_F2, $tabletLandscape_1280),
+    // а также kebab-case ($cor1-rh). Дефолт правила — строго kebab-case.
+    'scss/dollar-variable-pattern': '^-?[a-z][a-zA-Z0-9]*([-_][a-zA-Z0-9]+)*$',
+    'scss/at-mixin-pattern': '^-?[a-z][a-zA-Z0-9]*([-_][a-zA-Z0-9]+)*$',
+  },
 }
